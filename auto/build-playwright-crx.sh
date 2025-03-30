ecee45298ae415a6f520ec261de4011a956ac505
Hong Wu <hong.wu@tearline.xyz>
2025-03-30 11:37:45 +0800

    feat: improve build playwright-crx script

--- auto/build-playwright-crx.sh
+++ auto/build-playwright-crx.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-clone_playwright_crx() {
-  echo "Clone tearline-xyz/playwright-crx"
+git_clone_playwright_crx() {
+  echo "🗄️ Git clone tearline-xyz/playwright-crx"
 
   # Test which domain is accessible via SSH
   if ssh -T git@github.tearline -o BatchMode=yes -o ConnectTimeout=5 2>&1 | grep -q "successfully authenticated"; then
@@ -11,15 +11,15 @@ clone_playwright_crx() {
   elif ssh -T git@github.com -o BatchMode=yes -o ConnectTimeout=5 2>&1 | grep -q "successfully authenticated"; then
     DOMAIN="github.com"
   else
-    echo "Error: Unable to connect to either github.tearline or github.com via SSH"
+    echo "🚨 Error: Unable to connect to either github.tearline or github.com via SSH"
     exit 1
   fi
 
-  echo "Using domain: $DOMAIN"
+  echo "🔗 Using domain: $DOMAIN"
 
   git clone git@${DOMAIN}:tearline-xyz/playwright-crx.git
-  cd playwright-crx
 
+  cd playwright-crx
   # Rename the default remote to "tearline"
   git remote rename origin tearline
 
@@ -28,21 +28,26 @@ clone_playwright_crx() {
   cd -
 }
 
-
 echo '📍 Git clone playwright-crx if it does not exist'
 if [ -d "playwright-crx" ]; then
-  echo "playwright-crx directory already exists, skipping clone"
+  echo "📣 playwright-crx directory already exists, skipping git clone"
 else
-  clone_playwright_crx
+  git_clone_playwright_crx
 fi
 
 cd playwright-crx
-# Load nvm
+echo '🔨 Load nvm and use specified node.js version'
 \. $HOME/.nvm/nvm.sh
 nvm use
 
 echo '📍 Install playwright-crx dependencies'
+CORE_SOURCE_FILE="src/index.ts"
 npm install
+
+echo "📍 Running linter on ${CORE_SOURCE_FILE} to fix any issues"
+npx eslint --fix ${CORE_SOURCE_FILE}
 
 echo '📍 Build playwright-crx'
 npm run build:crx
+
+echo '🚩 Done'
